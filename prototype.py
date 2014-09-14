8982c4538fd5a212e798db12c151391af04fb793
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2014-09-14 17:50:38 -0400

    Improving prototyping

--- prototype.py
+++ prototype.py
@@ -1,3 +1,10 @@
 import subprocess
+import time
 
-subprocess.call(["scalac", "HelloWorld.scala"])
+def duration(i):
+  start = time.time()
+  subprocess.call(["scalac", "HelloWorld.scala"])
+  return time.time() - start
+
+durations = map(duration, xrange(5))
+print min(durations)
