14418fd6ca80227d1cb72b95320a557bb3ffce8f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2014-09-14 21:31:56 -0400

    More implicit tests

--- CodeGenerator.sh
+++ CodeGenerator.sh
@@ -6,7 +6,7 @@ for i in 10 20 30 40 50 60; do
 	scala ForCompCodeGen MultiForComp 3 $i > testsource/MultiForComp$i.scala
 done;
 
-for i in 10 20 30; do
-	j=`expr 40 - $i`  # number of implicits
+for i in 10 20 30 40 50; do
+	j=`expr 60 - $i`  # number of implicits
 	scala ImplicitCodeGen Implicit $i $j 20 > testsource/Implicit$j.scala
 done;
